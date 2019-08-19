cf8ac6d2881d69bb3933699e9373116c715c4e26
Your Name <you@example.com>
2019-08-19 13:50:24 +0900

    add heroku module to ap-server

--- blog/container/dockerfile/apserver/startup.sh
+++ blog/container/dockerfile/apserver/startup.sh
@@ -4,4 +4,9 @@ git clone -b initial_dev https://github.com/feivs2019/myblog.git myblog
 # python /opt/${PROJECT_NAME}/manage.py shell_plus --notebook
 python /opt/${PROJECT_NAME}/manage.py makemigrations
 python /opt/${PROJECT_NAME}/manage.py migrate
-python /opt/${PROJECT_NAME}/manage.py runserver 0.0.0.0:8000
+pip freeze > /opt/${PROJECT_NAME}/requirements.txt
+if [ $ENVIROMENT="PROD" ]; then
+    uwsgi --http :8001 --module blog.wsgi
+else
+    python /opt/${PROJECT_NAME}/manage.py runserver 0.0.0.0:8000
+fi
